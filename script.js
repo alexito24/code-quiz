f5304fb4a3cdd30cbbe46513361d0a9708977f63
alexito24 <87586398+alexito24@users.noreply.github.com>
2021-08-05 19:30:19 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,37 +1,144 @@
 const startButton = document.getElementById('start-btn')
-startButton.addEventListener('click', startGame)
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+const resultForm = document.getElementById('form-result');
 
+let countRightAnswers = 0;
+let shuffledQuestions, currentQuestionIndex
+let currentQuestion = 1;
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion();
+
+  currentQuestion++; 
+  document.getElementById('current-question').innerHTML = currentQuestion;
+})
 
 function startGame() {
-    startButton.classList.add('hide')
-    questionContainerElement.classList.remove('hide')
-    setNextQuestion()
+  document.getElementById('answer-buttons').classList.remove('no-click'); 
+
+  startButton.classList.add('hide')
+  resultForm.classList.add('hide');
+
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+
+  currentQuestion = 1;
+  document.getElementById('current-question').innerHTML = currentQuestion;
+
+  countRightAnswers = 0;
+
+  document.getElementById('all-questions2').innerHTML = questions.length;
+  document.getElementById('all-questions').innerHTML = questions.length;
 }
 
 function setNextQuestion() {
-
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
-function selectAnswer(){
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
 
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    resultForm.classList.remove('hide');
+    questionContainerElement.classList.add('hide');
+
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+
+  if (correct) {
+    countRightAnswers++; //+1
+  }
+
+  
+  document.getElementById('right-answers').innerHTML = countRightAnswers; 
+  document.getElementById('answers-percent').innerHTML = ((100 * countRightAnswers)/questions.length).toFixed(0);
+   
+  document.getElementById('answer-buttons').classList.add('no-click'); 
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
 }
 
 const questions = [
-    {
-        prompt: "what is 4 + 5?\n(a) 6\n\(b) 9\n(c) 10",
-        answer: "b"
-    },
-    {
-        prompt: "what is 10 * 2?\n(a) 12\n\(b) 22\n(c) 20",
-        answer: "c"
-    },
-    {
-        prompt: "what is 25 - 20?\n(a) 5\n\(b) 15\n(c) 10",
-        answer: "a"
-    },
-    {
-        prompt: "what is 12 + 8?\n(a) 20\n\(b) 15\n(c) 30",
-        answer: "a"
-    }
-
+  {
+    question: 'What is 2 + 2?',
+    answers: [
+      { text: '4', correct: true },
+      { text: '22', correct: false }
+    ]
+  },
+  {
+    question: 'What is 15 * 2?',
+    answers: [
+      { text: '10', correct: false },
+      { text: '30', correct: true },
+      { text: '17', correct: false },
+      { text: '11', correct: false }
+    ]
+  },
+  {
+    question: 'Is web development fun?',
+    answers: [
+      { text: 'Kinda', correct: false },
+      { text: 'YES!!!', correct: true },
+      { text: 'Um no', correct: false },
+      { text: 'IDK', correct: false }
+    ]
+  },
+  {
+    question: 'What is 4 * 2?',
+    answers: [
+      { text: '6', correct: false },
+      { text: '8', correct: true }
+    ]
+  }
 ]
+
